543825e8c55110a79ae724df092a5cec217aff66
or1g18 <or1g18@soton.ac.uk>
2019-02-09 14:30:56 +0000

    Added a basic Listener function that listens to the DragonServer (via the web app) with a websocket. The listener just prints out the message, for now.

---
+++ DragonServerListener.js
@@ -0,0 +1,8 @@
+pollServer = function(dragonServerAddress) {
+    conn = new WebSocket(dragonServerAddress + ":6363");
+
+    //just print out the message sent by the server
+    conn.onmessage = function (e) {
+        Console.log(e.data);
+    }
+};
