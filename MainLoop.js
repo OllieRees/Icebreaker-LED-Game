c933f0dd027ee37300242b9800aead514a72ea9c
or1g18 <or1g18@soton.ac.uk>
2019-02-09 13:41:09 +0000

    Added MainLoop.js and RoundLoop.js, which determine the behaviour of the game and round respectively. Also updated GenerateQuestion.js to become a class (may change this later down production).

---
+++ MainLoop.js
@@ -0,0 +1,10 @@
+/** Main loop for the game. */
+/** Determines the time limit of the game and question, and keeps each round going.*/
+
+class MainLoop {
+
+    roundTL = 0; //time limit for the round
+    gameTL = 0; //time limit for the game
+
+    /** Main game loop */
+}
