b0c29678db554f6be2af1b03670fc7cb74b98451
or1g18 <or1g18@soton.ac.uk>
2019-02-09 14:50:23 +0000

    removed js game logic files

--- RoundLoop.js
+++
@@ -1,4 +0,0 @@
-/** Determines when each question needs to be asked and by who. */
-/** Includes when each question needs to be asked and calls generateQuestion(), which generates a question a user to ask and who asks. */
-/** Sends a generated question to a phone screen */
-/** Sets a timed loop for the question to be answered */
