a3e8c859cc0b9f506913609613f4985777ead718
or1g18 <or1g18@soton.ac.uk>
2019-02-09 15:36:43 +0000

    Added a getAnswerUsername function to SendAnswer.js. This function gets the 'username' value stored on the page's cookie.

--- SendAnswer.js
+++ SendAnswer.js
@@ -5,11 +5,27 @@ getAnswer = function(TextFieldID) {
     return document.getElementById(TextFieldID).value;
 };
 
+/** Get username of answerer */
+getAnswererName = function() {
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let cookieFields = decodedCookie.split(";");
+  let username = ""; //default username
+  for (let fields in cookieFields) {
+      if (fields.contains("username")) {
+          username = fields.split(":")[1]; //remove username tag
+          username = username.substr(1, username.length); //remove any spaces at the beginning
+      }
+  }
+  return username;
+};
+
 /** Sends the answer inputted to the DragonServer */
 sendAnswer = function(TextFieldID, dragonServerAddress) {
     let answer = getAnswer(TextFieldID);
+    let username = getAnswererName();
     let conn = new WebSocket(dragonServerAddress + ":6363");
     conn.onopen = function() {
-        conn.send("Answer : " + answer);
+        conn.send("Username : " + username + ", Answer : " + answer);
     };
 };
+
