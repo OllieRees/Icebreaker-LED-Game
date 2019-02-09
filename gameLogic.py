446a8d05e613f57de159f20188a046b62a315b77
or1g18 <or1g18@soton.ac.uk>
2019-02-09 21:41:25 +0000

    Added some implementation to requestQuestion(self) and checkAnswer(self, question, answer). Also included some explanatory comments to gameLogic.py

--- gameLogic.py
+++ gameLogic.py
@@ -1,10 +1,11 @@
 import threading
+import random
 
 class iceBreaker():
 
     def __init__(self):
-        self.users = list()
-        self.questions = dict()
+        self.users = list() #just a list of users
+        self.questions = dict() #key is the user name, and the value is a dict of question:answer. 
 
     def resetGame(self):
         self.users = list()
@@ -16,11 +17,18 @@ class iceBreaker():
     def setActive(self, name):
         pass
 
+    #Request a question to send
     def requestQuestion(self):
-        pass
+        self.question = random.choice(self.questions)
+        #send question to be sent as HTML label
 
+    #check if the answer given is the same as the answer in the question-answer hashmap
     def checkAnswer(self, question, answer):
-        pass
+        questionAnswer = self.questions.get(activeUser) #gets the question and answer dict for the activeUser
+        if (answer == questionAnswer.get(question)): #check if the answer provided is the same as the one in the questions dict
+            return True
+        else:
+            return False
 
     def setQuestions(self, questionList):
         self.questions = questionList
