5b8f0031e0f048a16ff0c9a492b75d642170a0e0
or1g18 <or1g18@soton.ac.uk>
2019-02-10 11:28:26 +0000

    Removed deprecated files.

--- templates/DragonServerListener.js
+++
@@ -1,9 +0,0 @@
-/** Whenever the DragonServer sends a message to the webapp, print out the message */
-pollServer = function(dragonServerAddress) {
-    conn = new WebSocket(dragonServerAddress + ":6363");
-
-    //just print out the message sent by the server
-    conn.onmessage = function (e) {
-        Console.log(e.data);
-    }
-};
